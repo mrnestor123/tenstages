1fe7d86bf63dd441fd83b79d0e9dcbdb407c9b58
mrnestor123 <barrachinasanernest@gmail.com>
2023-02-05 12:31:47 +0100

    cambios

--- public/models/stage.js
+++ public/models/stage.js
@@ -3,6 +3,20 @@
 class StageEntity {
 
 
+    constructor(json){
+
+        this.newpathposition = json.newpathposition
+        this.type = json.type
+        this.title = json.title
+        this.image = json.image
+        this.stagenumber = json.stagenumber
+        this.position = json.position
+        this.cod = json.cod
+        this.text = json.text
+        this.description = json.description
+
+
+    }
 
 
 }
--- public/models/user.js
+++ public/models/user.js
@@ -8,43 +8,44 @@ class UserEntity {
     constructor(json){
         console.log('json',json)
         if(json){
-        this.coduser = json.coduser
-        this.nombre = json.nombre
-        this.position = json.position
-        this.image = json.image
-        this.stageNumber = json.stagenumber
-        this.stage = json.stage
-        this.offline = json.offline
-        this.role = json.role
-        this.meditPosition = json.meditposition
-        this.userStats = json.userStats
-        this.followed = json.followed
-        this.answeredQuestions = json.answeredquestions
-        this.gamePosition = json.gameposition
-        this.settings = json.settings
-        this.version = json.version
-        this.stageLessonsNumber = json.stagelessonsnumber
-        this.unreadMessages = json.unreadmessages
-        this.meditationTime = json.meditationTime
-        this.lastmeditDuration = json.lastmeditduration
-        this.website = json.website
-        this.teachingHours = json.teachinghours
-        this.location = json.location
-        this.description = json.description
-        this.seenIntroCarousel = json.seenIntroCarousel
-        this.reminderTime = json.reminderTime
-        
+            this.coduser = json.coduser
+            this.nombre = json.nombre
+            this.position = json.position
+            this.image = json.image
+            this.stageNumber = json.stagenumber
+            this.stage = json.stage
+            this.offline = json.offline
+            this.role = json.role
+            this.meditPosition = json.meditposition
+            this.userStats = json.userStats
+            this.followed = json.followed
+            this.answeredQuestions = json.answeredquestions
+            this.gamePosition = json.gameposition
+            this.settings = json.settings
+            this.version = json.version
+            this.stageLessonsNumber = json.stagelessonsnumber
+            this.unreadMessages = json.unreadmessages
+            this.meditationTime = json.meditationTime
+            this.lastmeditDuration = json.lastmeditduration
+            this.website = json.website
+            this.teachingHours = json.teachinghours
+            this.location = json.location
+            this.description = json.description
+            this.seenIntroCarousel = json.seenIntroCarousel
+            this.reminderTime = json.reminderTime
             
-        // listas
-        this.meditations = json.meditation || []
-        this.notifications = json.notifications || []
-        this.presets = json.presets || []
-        this.students = json.students || []
-        this.doneContent = json.doneContent || []
-    
-     }
+                
+            // listas
+            this.meditations = json.meditation || []
+            this.notifications = json.notifications || []
+            this.presets = json.presets || []
+            this.students = json.students || []
+            this.doneContent = json.doneContent || []
+        }
     }
 }
+
+
 
 // un user vacío para que sepa que es un objeto
 var user = new UserEntity();
